8b4e07ef27f04214851341b176fb7c8929cc1806
tuan nguyen <tuan.it303@gmail.com>
2025-10-01 00:40:09 +0000

    Remove device_id in dim_user

--- models/marts/dimensions/dim_user.sql
+++ models/marts/dimensions/dim_user.sql
@@ -1,7 +1,6 @@
 WITH source AS (
     SELECT DISTINCT
         user_id
-        ,device_id
         ,email_address
     FROM {{ ref('stg_user_behavior') }}
     WHERE user_id IS NOT NULL
@@ -9,6 +8,5 @@ WITH source AS (
 
 SELECT
     user_id
-    ,device_id
     ,email_address
 FROM source
